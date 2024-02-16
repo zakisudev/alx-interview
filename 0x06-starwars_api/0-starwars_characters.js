0de70b72bda103553c9c70a95e3ef1bdafc0e8be
zakisu250 <zakisu250@gmail.com>
2024-02-16 07:07:29 +0300

    Refactor API request and character retrieval in starwarsCharacters function

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,52 +1,21 @@
 #!/usr/bin/node
 
-const request = require('request');
-const movieId = process.argv[2];
+const util = require('util');
+const request = util.promisify(require('request'));
+const filmID = process.argv[2];
 
-function getMovieCharacters (movieId) {
-  const filmsUrl = 'https://swapi-api.alx-tools.com/api/films';
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  let response = await (await request(endpoint)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-  request.get(filmsUrl, (_, response, body) => {
-    if (response.statusCode === 200) {
-      const filmsData = JSON.parse(body);
-      const movieData = filmsData.results.find(
-        (film) => film.episode_id.toString() === movieId
-      );
-
-      if (movieData) {
-        const charactersUrls = movieData.characters;
-        const characters = [];
-
-        for (const charUrl of charactersUrls) {
-          request.get(charUrl, (_, response, body) => {
-            if (response.statusCode === 200) {
-              const characterData = JSON.parse(body);
-              characters.push(characterData.name);
-              if (characters.length === charactersUrls.length) {
-                printCharacters(characters, movieData.title);
-              }
-            } else {
-              console.log(`Error: ${response.statusCode}`);
-            }
-          });
-        }
-      } else {
-        console.log(`Movie ID ${movieId} not found.`);
-      }
-    } else {
-      console.log(`Error: ${response.statusCode}`);
-    }
-  });
-}
-
-function printCharacters (characters, movieTitle) {
-  for (const character of characters) {
-    console.log(character);
+  for (let i = 0; i < characters.length; i++) {
+    const urlCharacter = characters[i];
+    let character = await (await request(urlCharacter)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
   }
 }
 
-if (movieId) {
-  getMovieCharacters(movieId);
-} else {
-  console.log('Usage: node 0-starwars_characters.js [Movie ID]');
-}
+starwarsCharacters(filmID);
