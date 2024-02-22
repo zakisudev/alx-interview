bd8bcbb705fbc203577514317ca9d51bda78cd10
zakisu250 <zakisu250@gmail.com>
2024-02-22 11:39:58 +0300

    example removed for checker

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 0x07 - Rotate 2D Matrix
 """
@@ -6,7 +6,7 @@
 
 def rotate_2d_matrix(matrix):
     n = len(matrix)
-    
+
     # Transpose the matrix
     for i in range(n):
         for j in range(i, n):
@@ -15,15 +15,3 @@ def rotate_2d_matrix(matrix):
     # Reverse each row
     for i in range(n):
         matrix[i].reverse()
-
-# Example usage:
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-rotate_2d_matrix(matrix)
-
-for row in matrix:
-    print(row)
